a9014c0835591a9f41d7a0fd872c9720a5d56e3d
Bastien Dejean <nihilhill@gmail.com>
2015-07-12 10:30:37 +0200

    Add ProPhoto RGB workspace

--- dist/workspace.js
+++ dist/workspace.js
@@ -2,22 +2,31 @@
 
 var pow = Math.pow;
 
-var sRGBGamma = {
-	decode: function decode(v) {
-		return v <= 0.04045 ? v / 12.92 : pow((v + 0.055) / 1.055, 2.4);
+var sRgbGamma = {
+	decode: function decode(x) {
+		return x <= 0.04045 ? x / 12.92 : pow((x + 0.055) / 1.055, 2.4);
 	},
-	encode: function encode(v) {
-		return v <= 0.0031308 ? 12.92 * v : 1.055 * pow(v, 1 / 2.4) - 0.055;
+	encode: function encode(x) {
+		return x <= 0.0031308 ? 12.92 * x : 1.055 * pow(x, 1 / 2.4) - 0.055;
+	}
+};
+
+var proPhotoGamma = {
+	encode: function encode(x) {
+		return x < 0.001953125 ? 16 * x : pow(x, 1 / 1.8);
+	},
+	decode: function decode(x) {
+		return x < 16 * 0.001953125 ? x / 16 : pow(x, 1.8);
 	}
 };
 
 function simpleGamma(g) {
 	return {
-		decode: function decode(v) {
-			return pow(v, g);
+		decode: function decode(x) {
+			return pow(x, g);
 		},
-		encode: function encode(v) {
-			return pow(v, 1 / g);
+		encode: function encode(x) {
+			return pow(x, 1 / g);
 		}
 	};
 }
@@ -27,7 +36,7 @@ var workspaces = {
 		r: { x: 0.64, y: 0.33 },
 		g: { x: 0.3, y: 0.6 },
 		b: { x: 0.15, y: 0.06 },
-		gamma: sRGBGamma
+		gamma: sRgbGamma
 	},
 	"Adobe RGB": {
 		r: { x: 0.64, y: 0.33 },
@@ -40,6 +49,12 @@ var workspaces = {
 		g: { x: 0.1152, y: 0.8264 },
 		b: { x: 0.1566, y: 0.0177 },
 		gamma: simpleGamma(563 / 256)
+	},
+	"ProPhoto RGB": {
+		r: { x: 0.7347, y: 0.2653 },
+		g: { x: 0.1596, y: 0.8404 },
+		b: { x: 0.0366, y: 0.0001 },
+		gamma: proPhotoGamma
 	}
 };
 
--- src/workspace.js
+++ src/workspace.js
@@ -1,21 +1,31 @@
 var {pow} = Math;
 
-var sRGBGamma = {
-	decode: function (v) {
-		return (v <= 0.04045 ? v/12.92 : pow((v+0.055)/1.055, 2.4));
+var sRgbGamma = {
+	decode (x) {
+		return (x <= 0.04045 ? x/12.92 : pow((x+0.055)/1.055, 2.4));
 	},
-	encode: function (v) {
-		return (v <= 0.0031308 ? 12.92*v : 1.055*pow(v, 1/2.4)-0.055);
+	encode (x) {
+		return (x <= 0.0031308 ? 12.92*x : 1.055*pow(x, 1/2.4)-0.055);
+	}
+};
+
+
+var proPhotoGamma = {
+	encode (x) {
+		return (x < 0.001953125 ? 16 * x : pow(x, 1/1.8));
+	},
+	decode (x) {
+		return (x < 16 * 0.001953125 ? x / 16 : pow(x, 1.8));
 	}
 };
 
 function simpleGamma (g) {
 	return {
-		decode(v) {
-			return pow(v, g);
+		decode (x) {
+			return pow(x, g);
 		},
-		encode(v) {
-			return pow(v, 1/g);
+		encode (x) {
+			return pow(x, 1/g);
 		}
 	};
 }
@@ -25,7 +35,7 @@ var workspaces = {
 		r: {x: 0.64, y: 0.33},
 		g: {x: 0.30, y: 0.60},
 		b: {x: 0.15, y: 0.06},
-		gamma: sRGBGamma
+		gamma: sRgbGamma
 	},
 	"Adobe RGB": {
 		r: {x: 0.64, y: 0.33},
@@ -38,6 +48,12 @@ var workspaces = {
 		g: {x: 0.1152, y: 0.8264},
 		b: {x: 0.1566, y: 0.0177},
 		gamma: simpleGamma(563/256)
+	},
+	"ProPhoto RGB": {
+		r: {x: 0.7347, y: 0.2653},
+		g: {x: 0.1596, y: 0.8404},
+		b: {x: 0.0366, y: 0.0001},
+		gamma: proPhotoGamma
 	}
 };
 
